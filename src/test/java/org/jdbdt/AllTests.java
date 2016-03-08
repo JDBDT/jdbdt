ae5bf597c36e504ecdf500215bb68fa321bb4264
Eduardo Marques <eduardorbmarques@gmail.com>
2016-03-08 20:02:16 +0000

    AllTests: SQLlite suite was not enabled

--- src/test/java/org/jdbdt/AllTests.java
+++ src/test/java/org/jdbdt/AllTests.java
@@ -3,6 +3,7 @@ package org.jdbdt;
 import org.jdbdt.tcfg.derby.DerbySuite;
 import org.jdbdt.tcfg.h2.H2Suite;
 import org.jdbdt.tcfg.hsqlsb.HSQLDBSuite;
+import org.jdbdt.tcfg.sqlite.SQLiteSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -13,6 +14,7 @@ import org.junit.runners.Suite.SuiteClasses;
   DerbySuite.class,
   HSQLDBSuite.class,
   H2Suite.class,
+  SQLiteSuite.class
 })
 public class AllTests {
   
