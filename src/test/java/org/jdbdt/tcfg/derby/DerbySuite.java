f33172a9f596d9252af529214f77badcaa42bd63
Eduardo Marques <eduardorbmarques@gmail.com>
2016-03-08 15:16:28 +0000

    Derby test configuration simplified

--- src/test/java/org/jdbdt/tcfg/derby/DerbySuite.java
+++ src/test/java/org/jdbdt/tcfg/derby/DerbySuite.java
@@ -1,16 +1,14 @@
 package org.jdbdt.tcfg.derby;
 
+import org.jdbdt.DBEngineTestSuite;
 import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+
 
 @SuppressWarnings("javadoc")
-@RunWith(Suite.class)
-@SuiteClasses({ FileDBTest.class })
-public class DerbySuite {
+public class DerbySuite extends DBEngineTestSuite {
   @BeforeClass 
   public static void setup() throws ClassNotFoundException { 
     Class.forName("org.h2.Driver");
+    System.setProperty(DB_URL_PROP, "jdbc:derby:jdbdt-derby-test;create=true");
   }
 }
