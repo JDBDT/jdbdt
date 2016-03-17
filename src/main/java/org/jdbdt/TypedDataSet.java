81d0d7c51a92529d1fe5a75c9b864e0f4d1f8fd1
Eduardo Marques <eduardorbmarques@gmail.com>
2016-03-17 22:57:53 +0000

    TypedDataSet: now a final class

--- src/main/java/org/jdbdt/TypedDataSet.java
+++ src/main/java/org/jdbdt/TypedDataSet.java
@@ -9,7 +9,7 @@ import java.util.Collection;
  *
  * @since 0.1
  */
-public class TypedDataSet<T> extends DataSet {
+public final class TypedDataSet<T> extends DataSet {
 
   /**
    * Conversion function.
