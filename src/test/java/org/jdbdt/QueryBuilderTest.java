67386935ac21f72ac48a490492d39c13b10d7d84
Eduardo Marques <eduardorbmarques@gmail.com>
2016-05-06 10:25:00 +0100

    QueryBuilderTest: cleanup

--- src/test/java/org/jdbdt/QueryBuilderTest.java
+++ src/test/java/org/jdbdt/QueryBuilderTest.java
@@ -3,11 +3,9 @@ package org.jdbdt;
 
 import static org.jdbdt.JDBDT.*;
 import static org.jdbdt.TestUtil.*;
-import org.jdbdt.QueryBuilder.Param;
 
 import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map.Entry;
@@ -16,7 +14,6 @@ import java.util.function.Function;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
@@ -24,175 +21,6 @@ import org.junit.runners.MethodSorters;
 @SuppressWarnings("javadoc")
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class QueryBuilderTest extends DBTestCase { 
-
-  interface QMutator<T> {
-    Query set(T arg);
-  }
- // private QueryBuilder theSUT;
-  private EnumMap<QueryBuilder.Param,Object> qsetup;
-
-  @Before
-  public void createQuery() throws SQLException {
-//    theSUT = select(getDB(), UserDAO.COLUMNS)
-//            .from(UserDAO.TABLE_NAME);
-//    qsetup = new EnumMap<>(Param.class);
-//    qsetup.put(Param.FROM, UserDAO.TABLE_NAME);
-//    qsetup.put(Param.COLUMNS, UserDAO.COLUMNS);
-
-  }
-
-//  <T> void qset(Param p, QMutator<T> m, T arg) {
-//    qsetup.put(p, arg);
-//    assertSame(theSUT, m.set(arg));
-//  }
-//
-//  void qverify() {
-//    assertEquals(qsetup.get(S.WHERE), theSUT.whereClause());
-//    assertEquals(qsetup.get(S.HAVING), theSUT.havingClause());
-//    assertEquals(qsetup.get(S.DISTINCT), theSUT.distinctClause());
-//    assertArrayEquals((String[]) qsetup.get(S.GROUP_BY), theSUT.groupByClause());
-//    assertArrayEquals((String[]) qsetup.get(S.ORDER_BY), theSUT.orderByClause());
-//    assertArrayEquals((Object[]) qsetup.get(S.ARGS), theSUT.getQueryArguments());
-//    assertArrayEquals((String[]) qsetup.get(S.COLS), theSUT.getColumns());
-//  }
-
-
-//  @Test
-//  public void testInit() {
-//    qverify();
-//  }
-//
-//  @Test
-//  public void testInitWhere() {
-//    qset(S.WHERE, theSUT::where, "login='foo'");
-//    qverify();
-//  }
-//
-//  @Test
-//  public void testInitOrderBy1() {
-//    qset(S.ORDER_BY, theSUT::orderBy, new String[] { "login" });
-//    qverify();
-//  }
-//
-//  @Test
-//  public void testInitOrderBy2() {
-//    qset(S.ORDER_BY, theSUT::orderBy, new String[] { "password", "login" });
-//    qverify();
-//  }
-//
-//
-//  @Test
-//  public void testInitGroupBy1() {
-//    qset(S.GROUP_BY, theSUT::groupBy, new String[] { "password" });
-//    qverify();
-//  }
-//
-//  @Test
-//  public void testInitGroupBy2() {
-//    qset(S.COLS, theSUT::columns, new String[] { "password", "count(login)" });
-//    qset(S.GROUP_BY, theSUT::groupBy, new String[] { "password" });
-//    qverify();
-//  }
-//
-//  @Test
-//  public void testInitHaving() {
-//    qset(S.COLS, theSUT::columns, new String[] { "password", "count(login)" });
-//    qset(S.GROUP_BY, theSUT::groupBy, new String[] { "password" });
-//    qset(S.HAVING, theSUT::having,  "count(login) > 1" );
-//    qverify();
-//  }
-//
-//  @Test
-//  public void testInitQueryArguments() {
-//    qset(S.ARGS, theSUT::withArguments, new Object[] { "foo", 1 });
-//    qverify();
-//  }
-//
-//  @Test
-//  public void testInitDistinct() {
-//    qset(S.DISTINCT, dummy -> theSUT.distinct(), true);
-//    qverify();
-//  }
-//
-//  @Test
-//  public void testInitChain() {
-//    qset(S.COLS, theSUT::columns, new String[] { "password" });
-//    qset(S.WHERE, theSUT::where, "login LIKE ?");
-//    qset(S.ARGS, theSUT::withArguments, new Object[] { "foo%" });
-//    qset(S.ORDER_BY, theSUT::orderBy, new String[] { "password" });
-//    qset(S.DISTINCT, dummy -> theSUT.distinct(), true);
-//    qverify();
-//  }
-//
-//  void initTwice(QMutator<String> m) {
-//    m.set("1");
-//    expectException(InvalidOperationException.class, 
-//        () -> m.set("2"));
-//  }
-//
-//  @Test 
-//  public void testInitWhereTwice() { 
-//    initTwice(theSUT::where); 
-//  }
-//  @Test 
-//  public void testInitGroupByTwice() {
-//    initTwice(theSUT::groupBy);
-//  }
-//  @Test 
-//  public void testInitOrderByTwice() {
-//    initTwice(theSUT::orderBy);
-//  }
-//  @Test 
-//  public void testInitHavingTwice() {
-//    initTwice(theSUT::having);
-//  }
-//  @Test 
-//  public void testInitArgumentsTwice() {
-//    initTwice(theSUT::withArguments);
-//  }
-//  @Test 
-//  public void testInitColumnsTwice() {
-//    initTwice(theSUT::columns);
-//  }
-//  @Test 
-//  public void testInitDistinctTwice() {
-//    initTwice(dummy -> theSUT.distinct());
-//  }
-//
-//  void initAfterCompiling(QMutator<String> m) {
-//    theSUT.getQueryStatement();
-//    expectException(InvalidOperationException.class,
-//        () -> m.set("x"));
-//  }
-//
-//  @Test 
-//  public void testInitWhereAfterCompiling() { 
-//    initAfterCompiling(theSUT::where); 
-//  }
-//  @Test 
-//  public void testInitGroupByAfterCompiling() {
-//    initAfterCompiling(theSUT::groupBy);
-//  }
-//  @Test 
-//  public void testInitOrderByAfterCompiling() {
-//    initAfterCompiling(theSUT::orderBy);
-//  }
-//  @Test 
-//  public void testInitHavingAfterCompiling() {
-//    initAfterCompiling(theSUT::having);
-//  }
-//  @Test 
-//  public void testInitArgumentsAfterCompiling() {
-//    initAfterCompiling(theSUT::withArguments);
-//  }
-//  @Test 
-//  public void testInitColumnsAfterCompiling() {
-//    initAfterCompiling(theSUT::columns);
-//  }
-//  @Test 
-//  public void testInitDistinctAfterCompiling() {
-//    initAfterCompiling(dummy -> theSUT.distinct());
-//  }
 
   @Test
   public void testExecPlain() {
