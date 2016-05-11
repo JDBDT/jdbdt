c81fcd5f80bc632bfaca79e847b5ae15b5e92183
Eduardo Marques <eduardorbmarques@gmail.com>
2016-05-11 21:04:26 +0100

    DBSetup: whitespace adjustment

--- src/main/java/org/jdbdt/DBSetup.java
+++ src/main/java/org/jdbdt/DBSetup.java
@@ -147,7 +147,7 @@ final class DBSetup {
       
       DB db = table.getDB();
       db.logSetup(callInfo, sql);
-      PreparedStatement deleteStmt = db.compile(sql);   
+      PreparedStatement deleteStmt = db.compile(sql);
       if (args != null && args.length > 0) {
         for (int i=0; i < args.length; i++) {
           deleteStmt.setObject(i + 1, args[i]);
