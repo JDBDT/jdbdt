c97ee746b19ea242b94cf385699247c635db3ae9
Eduardo Marques <eduardorbmarques@gmail.com>
2016-05-27 19:59:39 +0100

    UserDAO: try-with-resource statements

--- src/test/java/org/jdbdt/UserDAO.java
+++ src/test/java/org/jdbdt/UserDAO.java
@@ -13,93 +13,86 @@ public class UserDAO {
     "login", "name", "password", "created" 
   };
 
-  private final PreparedStatement[] stmts; 
-    
+  private final Connection connection;
+
   public UserDAO(Connection c) throws SQLException {
-    Op[] ops = Op.values();
-    stmts = new PreparedStatement[ops.length];
+    connection = c;
     try {
-      stmts[0] = Op.DROP.compile(c);
-      stmts[0].execute();
+      Op.DROP.compile(c).execute();
+
     } catch(SQLException e) { }
-    stmts[1] = Op.CREATE.compile(c);
-    stmts[1].execute();
-    for (int i = 2; i < ops.length; i++) {
-      stmts[i] = ops[i].compile(c);
-    }
+    Op.CREATE.compile(c).execute();
   }
 
-  private PreparedStatement stmt(Op op) {
-    return stmts[op.ordinal()];
+  private PreparedStatement stmt(Op op) throws SQLException {
+    return op.compile(connection);
   }
 
   @SafeVarargs
   public final void doInsert(User... users) throws SQLException {
-    PreparedStatement s = stmt(Op.INSERT);
-    for (User u : users) {
-      s.setString(1, u.getLogin());
-      s.setString(2, u.getName());
-      s.setString(3, u.getPassword());
-      s.setDate(4, u.getCreated());
-      s.execute();
+    try(PreparedStatement s = stmt(Op.INSERT)) {
+      for (User u : users) {
+        s.setString(1, u.getLogin());
+        s.setString(2, u.getName());
+        s.setString(3, u.getPassword());
+        s.setDate(4, u.getCreated());
+        s.execute();
+      }
     }
   }
 
   public int doDeleteAll() throws SQLException {
-    return stmt(Op.DELETE_ALL).executeUpdate();
+    try (PreparedStatement stmt = stmt(Op.DELETE_ALL)) {
+      return stmt.executeUpdate();
+    }   
   }
 
   @SafeVarargs
   public final int doDelete(String... ids) throws SQLException {
     int n = 0;
-    PreparedStatement s = stmt(Op.DELETE);
-    for (String id : ids) {
-      s.setString(1, id);
-      n += s.executeUpdate();
+    try(PreparedStatement s = stmt(Op.DELETE)) {
+      for (String id : ids) {
+        s.setString(1, id);
+        n += s.executeUpdate();
+      }
+      return n;
     }
-    return n;
   }
 
   @SafeVarargs
   public final int doUpdate(User... users) throws SQLException {
     int n = 0;
-    PreparedStatement s = stmt(Op.UPDATE);
-    for (User u : users) {
-      s.setString(1, u.getName());
-      s.setString(2, u.getPassword());
-      s.setDate(3, u.getCreated());
-      s.setString(4, u.getLogin());
-      n += s.executeUpdate();
+    try (PreparedStatement s = stmt(Op.UPDATE)) {
+      for (User u : users) {
+        s.setString(1, u.getName());
+        s.setString(2, u.getPassword());
+        s.setDate(3, u.getCreated());
+        s.setString(4, u.getLogin());
+        n += s.executeUpdate();
+      }
     }
     return n;
   }
 
   public User query(String id) throws SQLException {
-    PreparedStatement s = stmt(Op.SELECT);
-    s.setString(1, id);
-    ResultSet rs = s.executeQuery();
-    try {
-      return rs.next() ? 
-          new User(id, 
-              rs.getString(1), 
-              rs.getString(2),
-              rs.getDate(3)) 
-      : null;
-    } 
-    finally {
-      rs.close();
+    try(PreparedStatement s = stmt(Op.SELECT)) {
+      s.setString(1, id);
+      try (ResultSet rs = s.executeQuery()) {
+        return rs.next() ? 
+            new User(id, 
+                rs.getString(1), 
+                rs.getString(2),
+                rs.getDate(3)) 
+        : null;
+      } 
     }
   }
 
   public int count() throws SQLException {
-    ResultSet rs = stmt(Op.COUNT).executeQuery();
-    rs.next();
-    try {
+    try(ResultSet rs = stmt(Op.COUNT).executeQuery()) {
+      rs.next();
       return rs.getInt(1);
     } 
-    finally {
-      rs.close();
-    }
   }
 
 
