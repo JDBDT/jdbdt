61fc524fbf34e3141734eafc92b58d244a343de5
Eduardo Marques <eduardorbmarques@gmail.com>
2016-08-11 19:20:06 +0100

    Regenerated

--- src/main/java/org/jdbdt/VersionInfo.java
+++ src/main/java/org/jdbdt/VersionInfo.java
@@ -9,7 +9,7 @@ final class VersionInfo {
   /**
    * Version Id.
    */
-  static final String ID = "0.1.0-SNAPSHOT";
+  static final String ID = "0.1.0-rc1";
   
   /**
    * Private constructor to avoid instantiation.
