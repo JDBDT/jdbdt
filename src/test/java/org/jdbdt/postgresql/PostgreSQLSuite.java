1b897299f0914e9a3e9ae0c746648b58f398b3af
Eduardo Marques <eduardorbmarques@gmail.com>
2017-05-08 04:24:31 +0100

    PostgreSQLSuite: simplified (but still no caching)

--- src/test/java/org/jdbdt/postgresql/PostgreSQLSuite.java
+++ src/test/java/org/jdbdt/postgresql/PostgreSQLSuite.java
@@ -7,41 +7,28 @@ import org.jdbdt.DBEngineTestSuite;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
-import ru.yandex.qatools.embed.postgresql.PostgresExecutable;
-import ru.yandex.qatools.embed.postgresql.PostgresProcess;
-import ru.yandex.qatools.embed.postgresql.PostgresStarter;
-import ru.yandex.qatools.embed.postgresql.config.PostgresConfig;
+import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
+import ru.yandex.qatools.embed.postgresql.distribution.Version;
+
 
 @SuppressWarnings("javadoc")
 public class PostgreSQLSuite extends DBEngineTestSuite {
-  
+  private static EmbeddedPostgres postgres;
+
   @BeforeClass 
   public static void setup() throws ClassNotFoundException, IOException { 
+    postgres = new EmbeddedPostgres(Version.V9_6_2);
+
+    String url = postgres.start("localhost",  5432, "jdbdt", "jdbt", "jdbdt");
+    
     DBConfig.getConfig()
-      .reset()
-      .setDriver("org.postgresql.Driver")
-      .setURL(startDatabase());
+    .reset()
+    .setDriver("org.postgresql.Driver")
+    .setURL(url);
   }
-  
+
   @AfterClass
   public static void teardown() {
-    stopDatabase();
-  }
-  
-  private static PostgresProcess process;
-
-  private static String startDatabase() throws IOException {
-    PostgresStarter<PostgresExecutable, PostgresProcess> runtime = PostgresStarter.getDefaultInstance();
-    final PostgresConfig config = PostgresConfig.defaultWithDbName("jdbdt-postgresql-test");
-    PostgresExecutable exec = runtime.prepare(config);
-    process = exec.start();
-    return String.format("jdbc:postgresql://%s:%s/%s",
-            config.net().host(),
-            config.net().port(),
-            config.storage().dbName());
-  }
-  
-  private static void stopDatabase() {
-    process.stop();
+    postgres.stop();
   }
 }
