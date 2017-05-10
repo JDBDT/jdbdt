478c4f79c89854816637b7a189ef7b47be1ed37f
Eduardo Marques <eduardorbmarques@gmail.com>
2017-05-11 00:37:21 +0100

    sonarque.sh: local run script update

--- scripts/sonarqube.sh
+++ scripts/sonarqube.sh
@@ -1,3 +1,4 @@
 mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar \
     -Dsonar.host.url=https://sonarqube.com \
-    -Dsonar.login=$SONARQUBE_TOKEN
+    -Dsonar.login=$SONARQUBE_TOKEN \
+    -Dsonar.organization=jdbdt
