e30d7a597a2962137a7c1bd449de5659a33ecd5c
Eduardo Marques <eduardorbmarques@gmail.com>
2017-05-13 18:58:29 +0100

    Full logging enabled for all tests (file output goes to 'jdbdt-log')

--- src/test/java/org/jdbdt/DBTestCase.java
+++ src/test/java/org/jdbdt/DBTestCase.java
@@ -1,5 +1,6 @@
 package org.jdbdt;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.SQLException;
@@ -20,32 +21,34 @@ public class DBTestCase {
   private static DB gDB;
   private static UserDAO gDAO;
   private static Conversion<User> gConversion;
-  
+
   protected static final 
   Conversion<User> STD_CONVERSION = 
-     u -> new Object[] {
-        u.getLogin(), 
-        u.getName(), 
-        u.getPassword(), 
-        u.getCreated()
-     };
+  u -> new Object[] {
+      u.getLogin(), 
+      u.getName(), 
+      u.getPassword(), 
+      u.getCreated()
+  };
   // Conversion when DATE is not supported (e.g. for SQLite)
   protected static final 
   Conversion<User> ALT_CONVERSION = 
-      u -> new Object[] {
-         u.getLogin(), 
-         u.getName(), 
-         u.getPassword(), 
-         u.getCreated() != null ? u.getCreated().getTime() : null
+  u -> new Object[] {
+      u.getLogin(), 
+      u.getName(), 
+      u.getPassword(), 
+      u.getCreated() != null ? u.getCreated().getTime() : null
   };
-  
+
   protected static Conversion<User> getConversion() {
     return gConversion;
   }
   protected static Object dateValue(Date d) {
     return gConversion == STD_CONVERSION ?  d : d.getTime();
   }
-      
+
+  static int gTestSuiteCounter = 0;
+
   @BeforeClass
   public static void setupDB() throws Exception {
     DBConfig cfg = DBConfig.getConfig();
@@ -59,10 +62,16 @@ public class DBTestCase {
       gDB.disable(DB.Option.REUSE_STATEMENTS);
     }
     newUserCounter = 0;
-    gDB.disable(DB.Option.LOG_ASSERTION_ERRORS);
-    // gDB.enableFullLogging();
+
+    File logDir = new File("jdbdt-log");
+    String fileName = String.format("%02d_%s.jdbdt.log",
+                                    ++gTestSuiteCounter,
+                                    cfg.getDriver());
+    logDir.mkdirs();
+    gDB.setLog(new File(logDir, fileName));
+    gDB.enableFullLogging();
   }
-  
+
   @AfterClass
   public static void teardownDB() throws SQLException {
     JDBDT.teardown(gDB, true);
@@ -72,17 +81,17 @@ public class DBTestCase {
   protected static DB getDB() {
     return gDB;
   }
-  
+
   protected static UserDAO getDAO() {
     return gDAO;
   }
 
   private static boolean useCustomInit = false;
-  
+
   static void useCustomInit() {
     useCustomInit = true;
   }
- 
+
   @Before
   public void populateByDefault() throws SQLException {
     if (!useCustomInit) {
@@ -90,33 +99,33 @@ public class DBTestCase {
       getDAO().doInsert(INITIAL_DATA);
     }
   }
-  
-  
+
+
   static final User[] INITIAL_DATA = {
-    new User("linus", "Linus Torvalds", "linux", Date.valueOf("2015-01-01")),
-    new User("steve", "Steve Jobs", "macos", Date.valueOf("2015-12-31")),
-    new User("bill", "Bill Gates", "windows", Date.valueOf("2015-09-12")),
-    new User("alanis", "Alanis ", "xyz", Date.valueOf("2015-01-01")),
-    new User("blanis", "Blanis ", "xyz", Date.valueOf("2015-01-02")),
-    new User("clanis", "Clanis ", "xyz", Date.valueOf("2015-01-03")),
-    new User("dlanis", "Dlanis ", "xyz", Date.valueOf("2015-01-04")),
-    new User("elanis", "Elanis ", "xyz", Date.valueOf("2015-01-05")),
-    new User("flanis", "Flanis ", "xyz", Date.valueOf("2015-01-06")),
-    new User("glanis", "Glanis ", "xyz", Date.valueOf("2015-01-07")),
-    new User("hlanis", "Hlanis ", "xyz", Date.valueOf("2015-01-08")),
-    new User("ilanis", "Ilanis ", "xyz", Date.valueOf("2015-01-09")),
-    new User("jlanis", "Jlanis ", "xyz", Date.valueOf("2015-01-10"))
+      new User("linus", "Linus Torvalds", "linux", Date.valueOf("2015-01-01")),
+      new User("steve", "Steve Jobs", "macos", Date.valueOf("2015-12-31")),
+      new User("bill", "Bill Gates", "windows", Date.valueOf("2015-09-12")),
+      new User("alanis", "Alanis ", "xyz", Date.valueOf("2015-01-01")),
+      new User("blanis", "Blanis ", "xyz", Date.valueOf("2015-01-02")),
+      new User("clanis", "Clanis ", "xyz", Date.valueOf("2015-01-03")),
+      new User("dlanis", "Dlanis ", "xyz", Date.valueOf("2015-01-04")),
+      new User("elanis", "Elanis ", "xyz", Date.valueOf("2015-01-05")),
+      new User("flanis", "Flanis ", "xyz", Date.valueOf("2015-01-06")),
+      new User("glanis", "Glanis ", "xyz", Date.valueOf("2015-01-07")),
+      new User("hlanis", "Hlanis ", "xyz", Date.valueOf("2015-01-08")),
+      new User("ilanis", "Ilanis ", "xyz", Date.valueOf("2015-01-09")),
+      new User("jlanis", "Jlanis ", "xyz", Date.valueOf("2015-01-10"))
   };
-  
+
   protected static final String EXISTING_DATA_ID1 =
       INITIAL_DATA[0].getLogin();
-  
+
   protected static final String EXISTING_DATA_ID2 =
       INITIAL_DATA[INITIAL_DATA.length/2].getLogin();
-  
+
   protected static final String EXISTING_DATA_ID3 =
       INITIAL_DATA[INITIAL_DATA.length-1].getLogin();
- 
+
   protected static User getTestData(String id) {
     for (User u : INITIAL_DATA) {
       if (u.getLogin().equals(id)) {
@@ -125,13 +134,13 @@ public class DBTestCase {
     }
     return null;
   }
-  
+
   protected Object[] rowFor(User u) {
     return getConversion().convert(u);
   }
-  
+
   private static int newUserCounter = 0;
-  
+
   protected static User buildNewUser() {
     final int unique = newUserCounter++;
     return new User("user" + unique, 
