6269a145ded4884aef9a0a62eba6c42697437050
Eduardo Marques <eduardorbmarques@gmail.com>
2017-06-01 03:41:14 +0100

    PostgreSQL - skipping embedded server in AppVeyor environment - #18

--- src/test/java/org/jdbdt/postgresql/PostgreSQLSuite.java
+++ src/test/java/org/jdbdt/postgresql/PostgreSQLSuite.java
@@ -1,36 +1,23 @@
 package org.jdbdt.postgresql;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-
 import org.jdbdt.DBConfig;
 import org.jdbdt.DBEngineTestSuite;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
-import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
-import ru.yandex.qatools.embed.postgresql.distribution.Version;
-
-
 @SuppressWarnings("javadoc")
 public class PostgreSQLSuite extends DBEngineTestSuite {
-  private static EmbeddedPostgres postgres;
 
   @BeforeClass 
-  public static void setup() throws ClassNotFoundException, IOException { 
-    postgres = new EmbeddedPostgres(Version.V9_6_2);
-    Path cachePath = FileSystems.getDefault().getPath(System.getProperty("user.home") + "/.embedpostgresql/");
-    String url = postgres.start(EmbeddedPostgres.cachedRuntimeConfig(cachePath));
-    
+  public static void setup() { 
     DBConfig.getConfig()
     .reset()
     .setDriver("org.postgresql.Driver")
-    .setURL(url);
+    .setURL(PostgresHandler.getInstance().start());
   }
 
   @AfterClass
   public static void teardown() {
-    postgres.stop();
+    PostgresHandler.getInstance().stop();
   }
 }
---
+++ src/test/java/org/jdbdt/postgresql/PostgresHandler.java
@@ -0,0 +1,58 @@
+package org.jdbdt.postgresql;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
+import ru.yandex.qatools.embed.postgresql.distribution.Version;
+
+@SuppressWarnings("javadoc")
+public enum PostgresHandler {
+  AppVeyor {
+    @Override
+    String start() {
+      return "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=Password12!";
+    }
+    @Override
+    void stop() { }
+  },
+  Embedded {
+    EmbeddedPostgres postgres;
+
+    @Override
+    String start() {
+      postgres = new EmbeddedPostgres(Version.V9_6_2);
+      Path cachePath = FileSystems.getDefault().getPath(System.getProperty("user.home") + "/.embedpostgresql/");
+      try {
+        String url = postgres.start(EmbeddedPostgres.cachedRuntimeConfig(cachePath));
+        System.out.println(url);
+        return url;
+      } catch (Exception e) {
+        throw new InternalError(e);
+      }
+    }
+
+    @Override
+    void stop() {
+      postgres.stop();
+    }
+
+  };
+
+  static PostgresHandler getInstance() {
+    String buildEnv = System.getenv("BUILD_ENVIRONMENT");
+    PostgresHandler handler = Embedded; // by default
+    if (buildEnv != null) {
+      try {
+        handler = Enum.valueOf(PostgresHandler.class, buildEnv);
+      } 
+      catch (IllegalArgumentException e) { 
+        // stick with default
+      }
+    }
+    return handler;
+  }
+
+  abstract String start();
+  abstract void stop();
+}
