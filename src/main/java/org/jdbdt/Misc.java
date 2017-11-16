45b47691a6cef249d396b1cf59f44af02cc96ac9
Eduardo Marques <eduardorbmarques@gmail.com>
2017-11-16 11:48:34 +0000

    Slight adjustment.

--- src/main/java/org/jdbdt/Misc.java
+++ src/main/java/org/jdbdt/Misc.java
@@ -18,7 +18,10 @@ final class Misc {
   private Misc() { } 
 
   @SuppressWarnings("javadoc")
-  private static final char[] HEX_CHARS = "0123456789abcdef".toCharArray();
+  private static final char[] HEX_CHARS = {
+      '0', '1', '2', '3', '4', '5', '6', '7', 
+      '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+  };
 
   /**
    * Convert byte array to a "hexa"-string.
