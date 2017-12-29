58f231e4a815c2feedea7ef6ab88a2ef2f1aa740
Eduardo Marques <eduardorbmarques@gmail.com>
2017-12-29 22:04:33 +0000

    Post-release cleanup [skip ci]

--- src/main/java/org/jdbdt/VersionInfo.java
+++ src/main/java/org/jdbdt/VersionInfo.java
@@ -9,7 +9,7 @@ final class VersionInfo {
   /**
    * Version Id.
    */
-  static final String ID = "1.0.3-SNAPSHOT";
+  static final String ID = "1.0.4-SNAPSHOT";
   
   /**
    * Private constructor to avoid instantiation.
