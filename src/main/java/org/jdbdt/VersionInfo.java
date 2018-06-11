9d8e9839b0ab709950840f607c53bec72e1f94e4
Eduardo Marques <eduardorbmarques@gmail.com>
2018-06-11 23:23:11 +0100

    Post-release cleanup

--- src/main/java/org/jdbdt/VersionInfo.java
+++ src/main/java/org/jdbdt/VersionInfo.java
@@ -9,7 +9,7 @@ final class VersionInfo {
   /**
    * Version Id.
    */
-  static final String ID = "1.0.8-SNAPSHOT";
+  static final String ID = "1.0.9-SNAPSHOT";
   
   /**
    * Private constructor to avoid instantiation.
--- src/main/java/org/jdbdt/package-info.java
+++ src/main/java/org/jdbdt/package-info.java
@@ -2,6 +2,6 @@
  * JDBDT (Java Database Delta Testing)
  * is a library for testing database applications.
  * 
- * @version 1.0.8-SNAPSHOT
+ * @version 1.0.9-SNAPSHOT
  */
 package org.jdbdt;
