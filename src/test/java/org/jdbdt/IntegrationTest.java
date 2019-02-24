ca6a3abfb70b7d275dc057fe76a1142cda589916
Eduardo Marques <edrdo@dcc.fc.up.pt>
2019-02-24 19:36:19 +0000

    pom.xml: rolling back dependency update for com.h2database:h2 - #55

--- src/test/java/org/jdbdt/IntegrationTest.java
+++ src/test/java/org/jdbdt/IntegrationTest.java
@@ -158,6 +158,7 @@ public class IntegrationTest extends DBTestCase {
              .build(getDB());
     takeSnapshot(q, table);
     getDAO().doInsert(u);
+    commit(getDB());
     assertInserted(data(q).row(newUserLogin), 
                    data(table, getConversion()).row(u));
     assertTrue(changed(table));
